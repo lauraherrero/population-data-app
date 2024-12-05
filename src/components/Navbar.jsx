30d51faa7ba8109e0152fa54bae0a67ff4e8e491
Laura Herrero Poza <lherrero@nextdigital.es>
2024-12-05 23:26:03 +0100

    Clear navbar file and delete comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,14 @@
-/* eslint-disable no-unused-vars */
 import { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-
 
 export const Navbar = () => {
-
 
     const [inputValue, setInputValue] = useState('');
 
     const handleChange = (e) => {
-        setInputValue(e.target.value);;
+        setInputValue(e.target.value);
     }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-4">
-            
-            {/* <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Population Data Query
-            </Link>
-
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-
-                    <NavLink 
-                        className={ ({isActive}) => `nav-item nav-link ${ isActive } ? 'active' : '' `} 
-                        to="/countries"
-                    >
-                        Countries
-                    </NavLink>
-
-                </div>
-            </div> */}
             <select name="regions" id="regions" value={inputValue} onChange={handleChange}>
                 <option value="Global">Global Population</option>
                 <option value="Africa">Africa</option>
