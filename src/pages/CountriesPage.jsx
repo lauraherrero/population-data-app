de48d225cd35201cf59155ab5c45c1a63d99edd3
Laura Herrero Poza <lherrero@nextdigital.es>
2024-12-05 17:58:42 +0100

    Add useEffect and fetch in countries page

--- src/pages/CountriesPage.jsx
+++ src/pages/CountriesPage.jsx
@@ -1,8 +1,27 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
 
 export const CountriesPage = () => {
+
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const countries = await response.json();
+        setData(countries);
+        } catch (error) {
+          setError(error);
+          }
+        };
+        fetchCountries();
+  }, []);
   return (
     <div>
       <h1>Countries Page</h1>
+      <input type="text" placeholder="Filter by population" />
     </div>
   )
 }
