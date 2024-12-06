31ace26fd08b9bc04eeca4e6bdba99c3a3366eae
Laura Herrero Poza <lherrero@nextdigital.es>
2024-12-06 08:14:39 +0100

    Create a new component for charts

---
+++ src/components/ChartPopulation.jsx
@@ -0,0 +1,43 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export const ChartPopulation = ({ data }) => {
+  const chartData = {
+    labels: data.map((d) => d.name),
+    datasets: [
+      {
+        label: "Population",
+        data,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 210, 220, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+  return (
+    <div>
+      <Bar data={chartData} options={chartOptions} />
+    </div>
+  );
+};
--- src/pages/ContinentsPage.jsx
+++ src/pages/ContinentsPage.jsx
@@ -2,15 +2,8 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+import { ChartPopulation } from "../components/ChartPopulation";
+
 
 export const ContinentsPage = () => {
   const [data, setData] = useState([]);
@@ -57,32 +50,6 @@ export const ContinentsPage = () => {
     setPopulation(value);
   };
 
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-
-  const chartData = {
-    labels: data.map(d => d.name),
-    datasets: [
-      {
-        label: "Population",
-        data,
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 210, 220, 1)",
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const chartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-  };
 
   return (
     <>
@@ -95,9 +62,7 @@ export const ContinentsPage = () => {
           onChange={handleFilterChange}
         />
       </div>
-      <div>
-        <Bar data={chartData} options={chartOptions} />
-      </div>
+      <ChartPopulation />
     </>
   );
 };
